13996476b5c2afbcc543860bcffe2c6494b59971
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2025-05-22 13:34:54 +0100

    conditional flow

--- app/_layout.js
+++ app/_layout.js
@@ -4,7 +4,7 @@ import {
 } from "@react-navigation/native";
 // import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { StyleSheet } from "react-native";
+import { Image, StyleSheet, Text } from "react-native";
 import Fashion from "./Fashion";
 import Instagram from "./Instagram";
 
@@ -13,9 +13,75 @@ const _layout = () => {
   return (
     <NavigationIndependentTree>
       <NavigationContainer>
-        <Tab.Navigator screenOptions={{ headerShown: false }}>
-          <Tab.Screen name="Fashion" component={Fashion} />
-          <Tab.Screen name="Instagram" component={Instagram} />
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Tab.Screen
+            name="Fashion"
+            component={Fashion}
+            options={{
+              tabBarLabel: ({ focused, color }) => {
+                return (
+                  <Text
+                    style={{
+                      color: focused ? "red" : "black",
+                      fontSize: focused ? 12 : 10,
+                    }}
+                  >
+                    Home
+                  </Text>
+                );
+              },
+              tabBarIcon: ({ focused, color, size }) => {
+                return (
+                  <Image
+                    source={{
+                      uri: "https://img.icons8.com/?size=100&id=73&format=png&color=000000",
+                    }}
+                    style={{
+                      height: focused ? 25 : 20,
+                      width: focused ? 25 : 20,
+                      tintColor: focused ? "red" : "black",
+                    }}
+                  />
+                );
+              },
+            }}
+          />
+          <Tab.Screen
+            name="Instagram"
+            component={Instagram}
+            options={{
+              tabBarLabel: ({ focused, color }) => {
+                return (
+                  <Text
+                    style={{
+                      color: focused ? "red" : "black",
+                      fontSize: focused ? 12 : 10,
+                    }}
+                  >
+                    Search
+                  </Text>
+                );
+              },
+              tabBarIcon: ({ focused, size, color }) => {
+                return (
+                  <Image
+                    source={{
+                      uri: "https://img.icons8.com/?size=100&id=132&format=png&color=000000",
+                    }}
+                    style={{
+                      height: focused ? 25 : 20,
+                      width: focused ? 25 : 20,
+                      tintColor: focused ? "red" : "black",
+                    }}
+                  />
+                );
+              },
+            }}
+          />
         </Tab.Navigator>
       </NavigationContainer>
     </NavigationIndependentTree>
